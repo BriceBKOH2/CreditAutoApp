ed456c7c0066e6b549947e6419b9db63b7115a1a
unknown <contact@jeanpierredurand.fr>
2019-12-05 18:14:54 +0100

    jpa config + table names

--- credit_auto_Java/src/main/java/com/bnpp/creditauto/model/Category.java
+++ credit_auto_Java/src/main/java/com/bnpp/creditauto/model/Category.java
@@ -1,6 +1,9 @@
 package com.bnpp.creditauto.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
@@ -10,7 +13,7 @@ import javax.persistence.Table;
  *
  */
 @Entity
-@Table(name="category")
+@Table(name="car_category_")
 public enum Category {
 	A("A"),
 	B("B"),
@@ -19,7 +22,25 @@ public enum Category {
 	E("E"),
 	F("F");
 	
+	@GeneratedValue
+	@Id
+	@Column(name="car_category_id")
+	private Long id;
+
+	@Column(name="car_category_name")
 	private String name;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 	Category(String name) {
 		this.name = name;
