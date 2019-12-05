a228516bc17c83851ae989e4adf62312996498b2
David Duveau <david.duveau@hotmail.fr>
2019-12-05 18:07:40 +0100

    form added to angular, no mechanics behind it

--- credit-auto-angular/src/app/calculation/calc-sheet/calc-sheet.component.ts
+++ credit-auto-angular/src/app/calculation/calc-sheet/calc-sheet.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-calc-sheet',
   templateUrl: './calc-sheet.component.html',
-  styleUrls: ['./calc-sheet.component.css']
+  styleUrls: ['./calc-sheet.component.scss']
 })
 export class CalcSheetComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
