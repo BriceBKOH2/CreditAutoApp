0917074c1f0f843edcd02396e1ae9dfb8f7a4743
David Duveau <david.duveau@hotmail.fr>
2019-12-05 19:07:53 +0100

    form bottstraped

--- credit-auto-angular/src/app/app.module.ts
+++ credit-auto-angular/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CalculationModule } from './calculation/calculation.module';
+import { NavigationModule } from './navigation/navigation.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CalculationModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CalculationModule,
+    NavigationModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- credit-auto-angular/src/app/navigation/navigation.module.ts
+++ credit-auto-angular/src/app/navigation/navigation.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent],
   imports: [
     CommonModule
   ]
