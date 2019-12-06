6f763f615de7fe728dadf4b9a24eb18566163573
David Duveau <david.duveau@hotmail.fr>
2019-12-06 13:48:32 +0100

    service updating

---
+++ credit-auto-angular/src/app/calculation/interface/loan.ts
@@ -0,0 +1,7 @@
+export interface Loan {
+  vehicle_cat: string;
+  vehicle_price: number;
+  loan_amount: number;
+  loan_duration: number;
+  rate: number;
+}
--- credit-auto-angular/src/app/calculation/service/simulation.service.ts
+++ credit-auto-angular/src/app/calculation/service/simulation.service.ts
@@ -1,17 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SimulationService {
-  constructor(private httpClient: HttpClient) {}
-  get endpoint() {
-    return /*path for api simulation made in Java language*/;
-  }
-
-  //   get httpOption(){headers: HttpHeaders}{
-  //   const headers = new HttpHeaders();
-  //   return{headers};
-  //   }
+  constructor() {}
 }
