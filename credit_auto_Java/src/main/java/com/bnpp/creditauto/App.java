cc459732f62554854f5c9bab04bff7bcc887d7cf
BriceB <brice.beteille@gmail.com>
2019-12-10 10:35:49 +0100

    bug resolved configuration on WebAppconfigurer

--- credit_auto_Java/src/main/java/com/bnpp/creditauto/App.java
+++ credit_auto_Java/src/main/java/com/bnpp/creditauto/App.java
@@ -1,39 +1,31 @@
 package com.bnpp.creditauto;
 
-import java.sql.Date;
-import java.time.LocalDate;
-
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.support.AbstractApplicationContext;
-
-import com.bnpp.creditauto.model.Client;
-import com.bnpp.creditauto.service.ClientService;
 
 @ComponentScan
 public class App {
 
 	public static void main(String[] args) {
 
-		try (AbstractApplicationContext context = new AnnotationConfigApplicationContext(App.class)) {
-			
-			ClientService cs = context.getBean(ClientService.class);
+//		try (AbstractApplicationContext context = new AnnotationConfigApplicationContext(App.class)) {
+//			
+//			ClientService cs = context.getBean(ClientService.class);
 //			cs.deleteAll();
-			System.out.println("Creation d'un client");
-
-			LocalDate ld = LocalDate.of(1989, 11, 02);
-			Date birth = Date.valueOf(ld);
-			// Création client Jordi
-			Client jordi = new Client("Jordi","Mage", birth,"phonenum","adresse 1", 123456789L);
-			cs.save(jordi);
-			// Création client David
-			Client david = new Client("David","Duveau", birth,"phonenum","adresse 2", 123456789L);
-			cs.save(david);
-			
-			System.out.println(cs.findAll());
-			
-			
-			// Test Client research method by firstname and lastname
+//			System.out.println("Creation d'un client");
+//
+//			LocalDate ld = LocalDate.of(1989, 11, 02);
+//			Date birth = Date.valueOf(ld);
+//			// Création client Jordi
+//			Client jordi = new Client("Jordi","Mage", birth,"phonenum","adresse 1", 123456789L);
+//			cs.save(jordi);
+//			// Création client David
+//			Client david = new Client("David","Duveau", birth,"phonenum","adresse 2", 123456789L);
+//			cs.save(david);
+//			
+//			System.out.println(cs.findAll());
+//			
+//			
+//			 //Test Client research method by firstname and lastname
 //			try {
 //				List<Client> clients = cs.findByNames("Jordi", "Mage");
 //				
@@ -44,7 +36,7 @@ public class App {
 //				// TODO Auto-generated catch block
 //				e.printStackTrace();
 //			}
-		}
+//		}
 	}
 
 }
--- credit_auto_Java/src/main/java/com/bnpp/creditauto/config/WebAppMvcConfigurer.java
+++ credit_auto_Java/src/main/java/com/bnpp/creditauto/config/WebAppMvcConfigurer.java
@@ -1,12 +1,12 @@
 package com.bnpp.creditauto.config;
 
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@WebAppConfiguration
+@Configuration
 @EnableWebMvc
 public class WebAppMvcConfigurer implements WebMvcConfigurer {
 	@Override
