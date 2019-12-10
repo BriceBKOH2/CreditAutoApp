c7fd754bbd373730864fafe61f763175bcf24757
unknown <contact@jeanpierredurand.fr>
2019-12-10 11:49:44 +0100

    resolved bug

--- credit_auto_Java/src/main/java/com/bnpp/creditauto/App.java
+++ credit_auto_Java/src/main/java/com/bnpp/creditauto/App.java
@@ -17,29 +17,29 @@ public class App {
 
 	public static void main(String[] args) {
 
-		try (AbstractApplicationContext context = new AnnotationConfigApplicationContext(App.class)) {
-			
-			ContractService cts = context.getBean(ContractService.class);
-			System.out.println("suppression du contenu de la table contract");
-			cts.deleteAll();
-			ClientService cls = context.getBean(ClientService.class);
-			System.out.println("Suppression du contenu de la table client");
-			cls.deleteAll();
-			
-			System.out.println("Creation d'un client");
-			Date d = new Date(System.currentTimeMillis());
-			Client client = new Client("coucou","test",d,"phonenum","adresse",123456789L);
-			cls.save(client);
-			System.out.println(cls.findAll());
-			
-			Category category = new Category("A");
-			category.setId(1L);
-			
-			System.out.println("Creation d'un contrat");
-			Contract contract = new Contract(9000L,8000L,24,0.65,true,d,client,category);
-			cts.save(contract);
-			System.out.println(cts.findAll());
-		}
+//		try (AbstractApplicationContext context = new AnnotationConfigApplicationContext(App.class)) {
+//			
+//			ContractService cts = context.getBean(ContractService.class);
+//			System.out.println("suppression du contenu de la table contract");
+//			cts.deleteAll();
+//			ClientService cls = context.getBean(ClientService.class);
+//			System.out.println("Suppression du contenu de la table client");
+//			cls.deleteAll();
+//			
+//			System.out.println("Creation d'un client");
+//			Date d = new Date(System.currentTimeMillis());
+//			Client client = new Client("coucou","test",d,"phonenum","adresse",123456789L);
+//			cls.save(client);
+//			System.out.println(cls.findAll());
+//			
+//			Category category = new Category("A");
+//			category.setId(1L);
+//			
+//			System.out.println("Creation d'un contrat");
+//			Contract contract = new Contract(9000L,8000L,24,0.65,true,d,client,category);
+//			cts.save(contract);
+//			System.out.println(cts.findAll());
+//		}
 	}
 
 }
