5277c57c507ccc33966f348d1f6589289eba7d1d
Jade <jade.accardo@gmail.com>
2019-12-10 12:16:36 +0100

    Ajout du CategoryController

---
+++ credit_auto_Java/src/main/java/com/bnpp/creditauto/controller/CategoryController.java
@@ -0,0 +1,36 @@
+package com.bnpp.creditauto.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bnpp.creditauto.model.Category;
+import com.bnpp.creditauto.service.CategoryService;
+
+@RestController
+@RequestMapping("/api/category")
+@CrossOrigin(origins = "http://localhost:4200")
+public class CategoryController {
+	
+	// Cette classe doit définir les methodes qui vont renvoyer du json
+	// Fera le lien entre le formulaire et le java.	
+	
+	@Autowired
+	private CategoryService categoryService;
+	
+	/**
+	 * Create and return an arbitrary rate.
+	 * For testing purposes.
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.GET)
+	@ResponseBody
+	public List<Category> findAll() {
+		return categoryService.findAll();
+	}
+}
