92d4f4acde8e4863217a7c337e5c217a5c46ad66
BriceB <brice.beteille@gmail.com>
2019-12-10 10:12:13 +0100

    pull successfull

--- credit_auto_Java/src/main/java/com/bnpp/creditauto/config/WebAppMvcConfigurer.java
+++ credit_auto_Java/src/main/java/com/bnpp/creditauto/config/WebAppMvcConfigurer.java
@@ -1,10 +1,12 @@
 package com.bnpp.creditauto.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 @EnableWebMvc
 public class WebAppMvcConfigurer implements WebMvcConfigurer {
 	@Override
