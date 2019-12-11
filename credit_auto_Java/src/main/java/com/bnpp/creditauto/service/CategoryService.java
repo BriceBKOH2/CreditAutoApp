ea41491c02a31bcc35232a3097997ca6df24d564
Jade <jade.accardo@gmail.com>
2019-12-11 11:14:09 +0100

    Ajout AbstractController

--- credit_auto_Java/src/main/java/com/bnpp/creditauto/service/CategoryService.java
+++ credit_auto_Java/src/main/java/com/bnpp/creditauto/service/CategoryService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.bnpp.creditauto.dao.CategoryDao;
 import com.bnpp.creditauto.model.Category;
+import com.bnpp.creditauto.model.Client;
 
 @Service
 public class CategoryService {
