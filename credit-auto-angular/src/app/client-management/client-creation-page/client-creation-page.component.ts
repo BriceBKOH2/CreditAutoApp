e4e17e65f3a498d8f7a26105ef9b98a3bdcf6560
BriceB <brice.beteille@gmail.com>
2019-12-16 12:59:30 +0100

    renamed variables in clientForm

--- credit-auto-angular/src/app/client-management/client-creation-page/client-creation-page.component.ts
+++ credit-auto-angular/src/app/client-management/client-creation-page/client-creation-page.component.ts
@@ -1,13 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ClientService } from '../service/client.service';
-import { Observable, BehaviorSubject } from 'rxjs';
-import { Contract } from 'src/app/calculation/class/contract';
-import { Category } from 'src/app/calculation/class/category';
 import { FormGroup, FormControl, Validators, NgForm } from '@angular/forms';
-import { Rate } from 'src/app/calculation/class/rate';
 import { Client } from 'src/app/calculation/class/client';
-import { map } from 'rxjs/operators';
-import { HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-client-creation-page',
@@ -15,40 +9,54 @@ import { HttpParams } from '@angular/common/http';
   styleUrls: ['./client-creation-page.component.scss']
 })
 export class ClientCreationPageComponent implements OnInit {
+  client: Client;
 
-    client: Client;
+  // clientForm$: BehaviorSubject<{client: Client}>;
 
-    //clientForm$: BehaviorSubject<{client: Client}>;
+  clientForm = new FormGroup({
+    clientLastName: new FormControl('', Validators.required),
+    clientFirstName: new FormControl('', Validators.required),
+    clientAccount: new FormControl('', Validators.required),
+    clientAddress: new FormControl('', Validators.required),
+    clientBirthDate: new FormControl('', Validators.required),
+    clientPhoneNumber: new FormControl('', Validators.required)
+  });
 
-    clientForm = new FormGroup({
-      clientlastname: new FormControl(''),
-      clientfirstname: new FormControl(''),
-      clientaccount: new FormControl(''),
-      clientaddress: new FormControl(''),
-      clientbirth: new FormControl(''),
-      clientphone: new FormControl(''),
-      });
-
-  constructor(private clientService: ClientService) { }
+  constructor(private clientService: ClientService) {}
 
   ngOnInit() {
-    //this.clientForm$ = new BehaviorSubject({
+    // this.clientForm$ = new BehaviorSubject({
     //  client: this.clientForm.get('client').value
-    //});
+    // });
+
+    this.client = new Client(
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined
+    );
+
+    this.clientService.getNewAccountNumber().subscribe(response => {
+      this.client.accountNumber = response;
+      this.clientForm.get('clientAccount').setValue(this.client.accountNumber);
+      console.log(this.client.accountNumber);
+    });
   }
 
-  onSubmitForm(){
+  onSubmitForm() {
     this.client = new Client(
-      this.clientForm.value.clientfirstname,
-      this.clientForm.value.clientlastname,
-      this.clientForm.value.clientbirth,
-      this.clientForm.value.clientphone,
-      this.clientForm.value.clientaddress,
-      this.clientForm.value.clientaccount
-      );
+      this.clientForm.value.clientFirstName,
+      this.clientForm.value.clientLastName,
+      this.clientForm.value.clientBirthDate,
+      this.clientForm.value.clientPhoneNumber,
+      this.clientForm.value.clientAddress,
+      this.clientForm.value.clientAccount
+    );
     this.clientService.putClient(this.client).subscribe(response => {
       this.client = response;
       console.log(this.client);
-    })
+    });
   }
 }
